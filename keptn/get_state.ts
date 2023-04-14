d7b883bf528de727b1e2ad067f8e972078743056
Thomas Schuetz <thomas.schuetz@t-sc.eu>
2023-04-14 16:40:41 +0200

    fix: style issues
    Signed-off-by: Thomas Schuetz <thomas.schuetz@t-sc.eu>

--- keptn/get_state.ts
+++ keptn/get_state.ts
@@ -20,11 +20,11 @@ let response = await fetch(url, {
         Accept: 'application/json',
     },
 })
-    .then(function(response) {
+    .then(function (response) {
         // The response is a Response instance.
         // You parse the data into a useable format using `.json()`
         return response.json();
-    }).then(function(data) {
+    }).then(function (data) {
         // `data` is the parsed version of the JSON returned from the above endpoint.
         if (data.infraReady) {
             console.log("Infrastructure is ready");
