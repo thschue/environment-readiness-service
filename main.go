a5ec03858f2dd236062bec9b81eccefeb023ba05
Thomas Schuetz <thomas.schuetz@t-sc.eu>
2023-04-14 23:04:23 +0200

    feat: change to relative backend path
    Signed-off-by: Thomas Schuetz <thomas.schuetz@t-sc.eu>

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import "github.com/thschue/environment-readiness-service/pkg/server"
 
 func main() {
-	serv := server.Config{Port: "8080"}
+	serv := server.Config{
+		Port: "8080",
+	}
 	err := serv.Serve()
 	if err != nil {
 		panic(err)
